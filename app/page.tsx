5b29ee206a4049bbbf41ad04b2cbdd5085b4fb80
Wahaj Hassan <wahajhassan007@gmail.com>
2025-09-06 14:32:54 +0500

    Make the Category Section and improve the perfomance of feature section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import FeaturedSection from "@/components/FeaturedSection";
+import CategorySection from "@/components/CategorySection";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main>
         <HeroSection/>
         <FeaturedSection/>
+        <CategorySection/>
       </main>
     </div>
   );
